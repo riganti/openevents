2acdc25eed8e7f45c4da753b5a2433dcba04dbdd
Tomas Herceg <tomas.herceg@riganti.cz>
2017-12-30 17:35:36 +0100

    API calls - time zone handling fixes

---
+++ src/OpenEvents/OpenEvents.Backend.Client/ApiClient.Partial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace OpenEvents.Backend.Client
+{
+    public partial class ApiClient
+    {
+
+        partial void UpdateJsonSerializerSettings(Newtonsoft.Json.JsonSerializerSettings settings)
+        {
+            settings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+        }
+
+    }
+}
